0d2bc21af420237c5e994f1df064e2c7bf48b6dd
Franco Franzini <francofranzini12@gmail.com>
2026-05-27 15:09:29 -0300

    Correcciones de push y hecho el ej 4

--- Practica4/minimo.c
+++ Practica4/minimo.c
@@ -14,27 +14,30 @@ void inicializar_array(){
     arr[i] = x;
     x = x + 0.2;
   }
+  arr[0] = 5.0;
+  arr[CANT_ELEMS-1] = 0.0;
 }
 
-
-void fsuma(){
-  
-  #pragma omp parallel for reduction(+:suma)
-  for(int i = 0; i < CANT_ELEMS; i++){
-    suma = suma + arr[i];
-  } 
-}
-void fsuma_serial(){
+void min_serial(){
+  int x = arr[0];
   for(int i = 0; i<CANT_ELEMS; i++){
-    suma_serial = suma_serial + arr[i];
+    x = x < arr[i] ? x : arr[i];
   }
 }
+void min_paralelo(){
+  int x = arr[0];
+  #pragma omp parallel for reduction(min: x)
+  for(int i = 0; i<CANT_ELEMS; i++){
+    if(arr[i] < x) x = arr[i];
+  } 
+}
+
 
 int main(){
   inicializar_array();
-  TIME_void(fsuma(), &t);
+  TIME_void(min_serial(), &t);
   printf("El tiempo paralelo fue: %.10f\n", t);      
-  TIME_void(fsuma_serial(), &t);
+  TIME_void(min_paralelo(), &t);
   printf("El tiempo serial fue: %.10f\n", t);        
   return 0;
 }
