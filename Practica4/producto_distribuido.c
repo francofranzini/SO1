55d1146ca572d10ce09f1b22cd7450722e1fb577
Franco Franzini <francofranzini12@gmail.com>
2026-06-01 19:50:16 -0300

    Terminado el 11

--- Practica4/producto_distribuido.c
+++ Practica4/producto_distribuido.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-#define N 6// definir
-int local_A[N][N];
-int main(int argc, char **argv)
-{
-	int i, j;
-	int A[N][N], v[N], x[N];
-	for(int i = 0; i<N; i++) {for(int j = 0; j<N; j++) A[i][j] = rand()%2; v[i] = 1;}
-	// printf("A is: \n");
-	// for(int i = 0; i<N; i++) {for(int j = 0; j<N; j++) printf("%i ",A[i][j]); printf("\n");}
-	
-	/*Leer A y v*/
-	int procesos, id;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &id);
-	MPI_Comm_size(MPI_COMM_WORLD, &procesos);
-	//Divido el array A de a N elementos
-	int lineas_por_proc = N / procesos; 
-	MPI_Scatter(&A, N*lineas_por_proc, MPI_INT, &local_A, N*lineas_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
+#include "timing.h"
 
-	int local_x[lineas_por_proc];
+#define N 5000// definir
+float t;
+double t1, t2, t3, t4;
+int i, j;
+int A[N][N], v[N], x[N];
+void calculo_paralelo(int id, int procesos, int* local_x, int* local_A, int lineas_por_proc){
+	//Divido el array A de a N elementos
+
+	MPI_Scatter(&A, N*lineas_por_proc, MPI_INT, local_A, N*lineas_por_proc, MPI_INT, 0, MPI_COMM_WORLD);
 	for(i=0; i<lineas_por_proc; i++){
 		local_x[i] = 0;
-
 		for(j=0; j<N; j++)
-			local_x[i] += local_A[i][j] * v[j];
+			local_x[i] += local_A[N*i + j] * v[j];
 	}
 	//Cada proceso calcula su porcion en local_x a partir de lo que se obtuvo en el scatter de A, en local_A
 	//Now we need to gather all the calculations into x
@@ -39,11 +29,50 @@ int main(int argc, char **argv)
 		0,
 		MPI_COMM_WORLD
 	);
-	MPI_Finalize();
-
-	for(int i = 0; i<N; i++) printf("%i ", x[i]);
-	printf("\n");
-
-	/*Escribir x */
+}
+int calculo_secuencial()
+{
+	/*Leer A y v*/
+	for (i=0;i<N;i++) {
+		x[i]=0;
+		for (j=0;j<N;j++)
+			x[i] += A[i][j]*v[j];
+	}
 	return 0;
 }
+
+int main(int argc, char **argv)
+{
+	for(int i = 0; i<N; i++) {for(int j = 0; j<N; j++) A[i][j] = rand()%2; v[i] = 1;}
+	TIME_void(calculo_secuencial(), &t); printf("El tiempo en secuencial fue: %.10f\n", t);
+
+
+	// MPI_Init(&argc, &argv);
+    // int rank, size;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	// MPI_Comm_size(MPI_COMM_WORLD, &size);
+	// t1 = MPI_Wtime();
+	// int lineas_por_proc = N / size; 
+	// int local_x[lineas_por_proc];
+	// int *local_A = malloc(lineas_por_proc * N * sizeof(int));
+
+    // calculo_paralelo(rank, size, local_x, local_A, lineas_por_proc);
+	// free(local_A);
+    // if(rank == 0){
+	// 	t2 = MPI_Wtime();
+    //     printf("El tiempo en paralelo fue: %.10f\n", t2 - t1);
+    // }
+
+    // MPI_Finalize();
+	
+	return 0;
+}
+/*
+	Registro de tiempos:
+	tiempo secuencial (ts) = 0.0347899310
+	1 core: 0.0763758070 --> Su = ts/(tp1) = 0.45
+	2 core: 0.0571015380 --> Su = ts/(tp2) = 0.60
+	3 core: 0.0498018470 --> Su = ts/(tp3) = 0.70
+
+
+*/
