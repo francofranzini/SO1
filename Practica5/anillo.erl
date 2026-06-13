ee09815c90d5005b0e89901cd21f964d6d22d5cc
Franco Franzini <francofranzini12@gmail.com>
2026-06-13 17:40:39 -0300

    Hecho el 5

--- Practica5/anillo.erl
+++ Practica5/anillo.erl
@@ -1,16 +1,41 @@
 -module(anillo).
 -export([init/0]).
 
-proceso(Pid) ->
+proceso() ->
     receive
-        {msg, N} -> 
-
+        {next, Pidnext} -> loopProc(Pidnext)
+    end.
+loopProc(Pidnext) ->
+    receive
+        {msg, 0} ->
+            io:fwrite("Me llego el 0 y soy ~p ~n", [self()]),
+            Pidnext ! exit,
+            exit(normal);
+        {msg, N} ->
+            io:fwrite("Soy ~p y me llego ~p~n", [self(), N]),
+            Pidnext ! {msg, N-1},
+            loopProc(Pidnext);
+        exit ->
+            Pidnext ! exit,
+            exit(normal)
     end.
 
 init() ->
     Nprocs = 5,
     Valor = 15,
-    Pids = [spawn(fun proceso/1) || _ <- 1..Nprocs],
-    Pids[i] ! Pids[i+1],
-    Pids[Nprocs] ! Pids[1],
-    hd(Pids) ! {Hola, Valor}.
+    Pids = [spawn(fun proceso/0) || _ <- lists:seq(1, Nprocs)], %crea los procesos sin sucesor
+    encadenar(Pids),
+    Monitores = [erlang:monitor(process, P) || P <- Pids],
+    hd(Pids) ! {msg, Valor},
+    [receive{'DOWN', Ref, process, _, _} -> ok end || Ref <- Monitores],
+    io:format("Todos los procesos terminaron correctamente.~n").
+
+
+encadenar(Pids) ->
+    encadenar(Pids, hd(Pids)).
+encadenar([Pid|Rest], First) ->
+    case Rest of
+        [] -> Pid ! {next, First};
+        [Next | _] -> Pid ! {next, Next}, encadenar(Rest, First)
+    end.
+
